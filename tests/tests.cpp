6194eec07152d2a428befbdeac6f9df37767edfa
Jules Fouchy <jules.fouchy@ntymail.com>
2023-02-28 21:10:16 +0100

    ✅ Check that we successfully found the version

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,7 +1,13 @@
+#include <stdlib.h>
 #include <iostream>
 #include <os_name/os_name.hpp>
 
 auto main() -> int
 {
-    std::cout << Cool::os_name() << '\n';
+    auto const& str = Cool::os_name();
+    std::cout << str << '\n';
+
+    if (str.find("Unknown") != std::string::npos)
+        return EXIT_FAILURE; // Error if we failed to get the exact version.
+    return EXIT_SUCCESS;
 }
